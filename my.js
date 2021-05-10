4c32bfde218875dc85ab3e4de9eecd654c88ca6b
MohammedMajid88 <mohamedmajed8888@gmail.com>
2021-05-10 13:13:43 +0300

    this assignment12 not completed yet

---
+++ my.js
@@ -0,0 +1,50 @@
+function containsZero(arry) {
+    for (let i = 0; i =  arry.lenght; i++) {
+        if(arrry[i] == 0)
+        return true;
+    }
+    return false;
+}
+
+
+
+
+function allPasitive(arry) {
+    for (let i = 0; i < array.length; i++) {
+        if (arry[i] < 0)
+        return false;
+        
+    }
+    return true;
+}
+
+
+
+
+const array = ['a', 'b', 'c']
+function joinStrings(array, separator) {
+    console.log(array.join(separator));
+   
+   
+}
+joinStrings(array, '*');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
